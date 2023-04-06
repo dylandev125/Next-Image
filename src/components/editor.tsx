9f3bdbc10a88e4e84d48153f9c50e820d0d0f353
dylandev125 <pauleta0126@gmail.com>
2023-04-06 05:05:39 +0200

    fix image styling

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -8,13 +8,10 @@ interface EditorTypes {
   onChangeImage: (source: string, url: string) => void;
 }
 
-const ImageEditor = ({
-  source,
-  open,
-  onChangeImage
-}: EditorTypes) => {
+const ImageEditor = ({ source, open, onChangeImage }: EditorTypes) => {
   const canvasRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [imageInfo, setImageInfo] = useState<any>({ width: 1, height: 1 });
 
   useEffect(() => {
     const canvas: any = canvasRef.current;
@@ -27,6 +24,7 @@ const ImageEditor = ({
       // draw the image on the canvas
       canvas.width = img.width;
       canvas.height = img.height;
+      setImageInfo({ width: img.width, height: img.height });
       ctx.drawImage(img, 0, 0);
     };
   }, [source, open]);
@@ -63,28 +61,33 @@ const ImageEditor = ({
       const url = URL.createObjectURL(blob);
       onChangeImage(source, url);
     }, "image/png");
-  }
+  };
 
+  console.log(imageInfo);
   return (
     <>
-
-    <div className={`${open ? "block" : "hidden"} min-w-[calc(50%)] h-[calc(100%-150px)] fixed top-[50%] left-[50%] bottom-auto !-translate-x-[50%] !-translate-y-[50%] -mr-[50%] z-50`}>
-      <canvas
-        className="absolute top-0 left-0 w-full h-full z-1"
-        ref={canvasRef}
-        onMouseDown={startDrawing}
-        onMouseUp={stopDrawing}
-        onMouseMove={draw}
-      />
-      <Button
-        className="bg-[#2b2dbd] text-white text-xl font-bold border border-[#2b2dbd] hover:bg-white hover:text-[#2b2dbd] absolute bottom-0"
-        onClick={onClickSave}
+      <div
+        className={`${
+          open ? "block" : "hidden"
+        } md:h-[calc(100%-150px)] md:w-auto w-[80%] h-auto fixed top-[50%] left-[50%] bottom-auto !-translate-x-[50%] !-translate-y-[50%] -mr-[50%] z-50`}
+        style={{ aspectRatio: `${imageInfo.width / imageInfo.height}` }}
       >
-        Save
-      </Button>
-    </div>
+        <canvas
+          className="absolute top-0 left-0 w-full h-full z-1"
+          ref={canvasRef}
+          onMouseDown={startDrawing}
+          onMouseUp={stopDrawing}
+          onMouseMove={draw}
+        />
+        <Button
+          className="bg-[#2b2dbd] text-white text-xl font-bold border border-[#2b2dbd] hover:bg-white hover:text-[#2b2dbd] absolute bottom-0"
+          onClick={onClickSave}
+        >
+          Save
+        </Button>
+      </div>
     </>
   );
-}
+};
 
 export default ImageEditor;
