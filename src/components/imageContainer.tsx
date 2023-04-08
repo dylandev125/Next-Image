6b104ef946d8b5a57c33dd755b6ad3647ce251d6
dylandev125 <pauleta0126@gmail.com>
2023-04-08 14:24:12 +0200

    image layout

--- src/components/imageContainer.tsx
+++ src/components/imageContainer.tsx
@@ -20,17 +20,17 @@ const ImageContainer = ({
         handleRequestOpen();
     }
     return (
-        <div className="relative border-2">
+        <div className="relative border-2 group overflow-hidden">
             <Image alt="" width={600} height={0} src={source}></Image>
-            <div className="h-[60px] absolute bottom-0 flex w-full justify-between px-4 pb-4">
+            <div className="h-full absolute -bottom-[calc(100%)] group-hover:bottom-0 flex w-full justify-between items-end px-4 pb-4 bg-gradient-to-t from-black via-[#00000044] to-[#00000000] transition-all">
                 <Button
-                    className="bg-lime-800 text-white hover:bg-[white] border hover:border-lime-800 hover:text-black"
+                    className="bg-lime-800 text-white hover:bg-[white] border hover:border-lime-800 hover:text-black h-14"
                     onClick={onImageEdit}
                 >
                     Edit
                 </Button>
                 <Button
-                    className="bg-red-500 text-white hover:bg-[white] border hover:border-red-500 hover:text-black"
+                    className="bg-red-500 text-white hover:bg-[white] border hover:border-red-500 hover:text-black h-14"
                     onClick={onRequestEdit}
                 >
                     Request Edit
